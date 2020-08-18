f4d72d3c930308bb269705b8c7d7c46417e1ae11
Jessica <contact.jessicasanchez@gmail.com>
2020-08-18 10:59:06 -0400

    Authorization logging scopes

--- src/app.js
+++ src/app.js
@@ -3,19 +3,53 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.port || 3001; //if deploying to like heroku this will make sure the random port is used
+const PORT = process.env.PORT || 3001; //if deploying to like heroku this will make sure the random port is used
+const session = require('express-session')
+const passport = require('passport');
+const discordStrategy = require('./strategies/discordStrategy');
+/*----------  Routes  ----------*/
+const authRouter = require('./routes/auth');
 
-app.get("/", (req, res) => {
-  res.send("Hello"); //make a request and then send that response
-})
+app.use(session({
+  secret: '9470840216',
+  cookie: {
+    maxAge: 60000 * 60 * 24
+  },
+  saveUninitialized: false
+}));
 
 
-//by doing another 
-app.get("/dashboard",(req, res) => {
+/*----------  Passport  ----------*/
 
-})
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*----------  Middleware & Routes  ----------*/
+app.use('/auth', authRouter);
 
 app.listen(PORT, () => {
   console.info(`Listening on port ${PORT}`)
 });
+
+
+
+
+
+
+/*=============================================
+=            EXPLANATION OF GET            =
+=============================================*/
+
+// app.get("/", (req, res) => {
+//   res.send("Hello"); //*make a request and then send that response
+// })
+ 
+ //*by doing another get with a route it is literally making those routes for me
+// app.get("/dashboard", (req, res) => {
+  //things that you want to do with the response
+// })
+
+
+/*=====  End of EXPLANATION OF GET  ======*/
+
 
