43df701f310ccb4bca908bf51a17a20f844811f6
Jessica <contact.jessicasanchez@gmail.com>
2020-08-19 22:11:01 -0400

    getting unauthorized

--- src/models/DiscordUser.js
+++ src/models/DiscordUser.js
@@ -3,8 +3,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  discordId: {type: String, required: true},
-  username: {type: String, required: true}
+  discordId: {type: String, required: true, unique: true},
+  discordTag: {type: String, required: true},
+  avatar: {type: String, required: true},
+  guilds: {type: Array, required: true},
 });
 
 const DiscordUser = module.exports = mongoose.model('User', UserSchema);
